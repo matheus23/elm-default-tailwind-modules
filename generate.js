a150128b56bf8fe1b344ebae071aec37f98433e5
Philipp Krüger <philipp.krueger1@gmail.com>
2023-02-05 17:43:30 +0100

    :rocket: Use elm-tailwind-modules 0.5.0-alpha1

--- generate.js
+++ generate.js
@@ -9,19 +9,25 @@ import lockfile from "@yarnpkg/lockfile";
 
 // Find out which tailwind version is used
 
-const yarnLockFile = fs.readFileSync("yarn.lock", "utf8");
-const yarnLock = lockfile.parse(yarnLockFile).object;
-const pkgJson = fs.readFileSync("package.json", "utf8");
-const pkg = JSON.parse(pkgJson);
+let tailwindVersion, elmTailwindModulesVersion;
+try {
 
-const tailwindVersion = yarnLock[`tailwindcss@${pkg.dependencies.tailwindcss}`].version;
+    const yarnLockFile = fs.readFileSync("yarn.lock", "utf8");
+    const yarnLock = lockfile.parse(yarnLockFile).object;
+    const pkgJson = fs.readFileSync("package.json", "utf8");
+    const pkg = JSON.parse(pkgJson);
+    
+    tailwindVersion = yarnLock[`tailwindcss@${pkg.dependencies.tailwindcss}`].version;
+    elmTailwindModulesVersion = yarnLock[`elm-tailwind-modules@${pkg.dependencies["elm-tailwind-modules"]}`].version;
+} catch {
+    console.error("Couldn't figure out tailwind and/or elm-tailwind-modules version. Make sure to run yarn install first.");
+    process.exit(1);
+}
 
-console.log(`Generating for tailwindcss version ${tailwindVersion}`);
+console.log(`Generating for tailwindcss version ${tailwindVersion} using elm-tailwind-modules version ${elmTailwindModulesVersion}.`);
 
 
-// Create a custom documentation generator that generates
-// 1. less text (we're getting close to elm package upload restrictions)
-// 2. contains the tailwind version used
+// Create a custom documentation generator that contains the tailwind version used
 
 const docGen = {
     ...elmTailwindModules.docs.defaultDocumentationGenerator,
@@ -39,15 +45,9 @@ This module contains
     You can browse the documentation on
     [tailwind's website](https://tailwindcss.com/docs)
 
+These utilities were generated using [elm-tailwind-modules](https://github.com/matheus23/elm-tailwind-modules) version ${elmTailwindModulesVersion}.
+
 ${definedNames.map(c => `@docs ${c}`).join("\n")}
-
--}
-`,
-
-    utilitiesDefinition: (_elmName, declaration) => `
-{-| The tailwind class \`${declaration.originalClassName}\`.
-
-Make sure to check out the [tailwind documentation](https://tailwindcss.com/docs)!
 
 -}
 `,
